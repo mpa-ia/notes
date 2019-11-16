693953f520b6c84ebe1bebc630bdf11fa4bdc3a8
Marysia Pabich <marysia.pabich@gmail.com>
2019-11-16 11:52:06 +0100

    Add functionality to create new column

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,7 @@ import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js';
 
 
 class List extends React.Component {
@@ -20,6 +21,23 @@ class List extends React.Component {
   static defaultProps = {
     description: settings.defaultListDescription,
   }
+  
+  addColumn(title){
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
+
   render() {
     return (
       <section className={styles.component}>
@@ -32,6 +50,9 @@ class List extends React.Component {
             <Column key={key} {...columnProps} />
           ))}
        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
+        </div>
       </section>
     )
   }
