df6aecb351c964e40eb8d39e5faaab085ab1ea71
Marysia Pabich <marysia.pabich@gmail.com>
2019-11-17 12:20:08 +0100

    Introduce Card inside Column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card.js';
+import Creator from '../Creator/Creator.js';
+import {settings} from '../../data/dataStore';
 
 class Column extends React.Component {
 
-    static propTypes = {
-      title: PropTypes.string,
-    }
+  state = {
+    cards: this.props.cards || [],
+  }
+  static propTypes = {
+    title: PropTypes.string,
+  }
+
+  addCard(title){
+    this.setState(state => (
+      {
+        cards: [
+          ...state.cards,
+          {
+            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
+            title,
+          }
+        ]
+      }
+    ));
+  }
 
   render() {
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{this.props.title}</h3>
+        <div className={styles.component}>                          {/* klasa */}
+          {this.state.cards.map(({ key, ...columnProps }) => (
+            <Card key={key} {...columnProps} />
+          ))}
+       </div>
+       <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        </div>
       </section>
     )
   }
