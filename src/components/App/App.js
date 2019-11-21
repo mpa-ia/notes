d3b87221c0c77b0b65112254d55f872f6fb2e5d1
Marysia Pabich <marysia.pabich@gmail.com>
2019-11-21 10:30:00 +0100

    Introduce Redux, adjust components to take data from store

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import styles from './App.scss';
-import List from '../List/List.js';
-import {pageContents, listData, settings} from '../../data/dataStore';
-import Creator from '../Creator/Creator.js';
+import List from '../List/ListContainer.js';
+import {listData} from '../../data/dataStore';
+// import Creator from '../Creator/Creator.js';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
   state = {
     lists: listData || [],
   }
-  addList(title, description, image) {
-    this.setState(state => (
-      {
-        lists: [
-          ...state.lists, 
-          {
-            key: state.lists.length ? state.lists[state.lists.length-1].key+1 : 0,
-            title,
-            description: description || settings.defaultListDescription,
-            image: image || settings.defaultHeroImage,
-            columns: [],
-          },
-        ],
-      }
-    ));
+
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+    lists: PropTypes.array,
   }
   render() {
+    const {title, subtitle, lists} = this.props;
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
         <div className={styles.columns}>
-          {this.state.lists.map(({key, ...listProps}) => (
-            <List key={key} {...listProps} />
+          {lists.map(listData => (
+            <List key={listData.id} {...listData} />
           ))}
         </div>
-        <div className={styles.creator}>
+        {/* <div className={styles.creator}>
           <Creator text={settings.listCreatorText} action={title => this.addList(title)} />
-        </div>
+        </div> */}
       </main>
     );
   }
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,52 +2,35 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-import Creator from '../Creator/Creator.js';
-import {settings} from '../../data/dataStore';
+// import Creator from '../Creator/Creator.js';
+// import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
 
-  state = {
-    cards: this.props.cards || [],
-  }
   static propTypes = {
     title: PropTypes.string,
     icon: PropTypes.string,
     cards: PropTypes.array,
   }
 
-  addCard(title){
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-            title,
-          },
-        ],
-      }
-    ));
-  }
-
   render() {
+    const {title, icon, cards} = this.props;
+
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>
-          {this.props.title}
+          {title}
           <span className={styles.icon}>
-            <Icon name={this.props.icon}/>
+            <Icon name={icon}/>
           </span>
-        </h3>
-        <div className={styles.component}>
-          {this.state.cards.map(({ key, ...columnProps }) => (
-            <Card key={key} {...columnProps} />
-          ))}
-        </div>
-        <div className={styles.creator}>
+        </h3>   
+        {cards.map(cardData => (
+          <Card key={cardData.id} {...cardData} />
+        ))}
+        {/* <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div>
+        </div> */}
       </section>
     );
   }
