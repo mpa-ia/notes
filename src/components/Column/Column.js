461b27598662723dc11d61833b182468e0c75c18
Marysia Pabich <marysia.pabich@gmail.com>
2019-11-22 08:32:40 +0100

    Use Redux to perform AddCard action

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-// import Creator from '../Creator/Creator.js';
+import Creator from '../Creator/Creator.js';
 import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
@@ -12,6 +12,7 @@ class Column extends React.Component {
     title: PropTypes.string,
     icon: PropTypes.string,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
   }
   
   static defaultProps = {
@@ -19,7 +20,7 @@ class Column extends React.Component {
   }
   
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
 
     return (
       <section className={styles.component}>
@@ -32,9 +33,9 @@ class Column extends React.Component {
         {cards.map(cardData => (
           <Card key={cardData.id} {...cardData} />
         ))}
-        {/* <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div> */}
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={addCard} />
+        </div>
       </section>
     );
   }
