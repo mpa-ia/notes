57d34b68c3ca583df91ec7063c5991e4861e0b20
Marysia Pabich <marysia.pabich@gmail.com>
2019-11-27 18:59:57 +0100

    Add navigation bar, functionality to switch between views (connect new components)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
+import Info from '../Info/Info';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MainLayout from '../MainLayout/MainLayout';
+import Header from '../Header/Header';
 
 
 const App = () => (
-  <div>
-    <Home />
-  </div>
+  <MainLayout>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/info' component={Info} />
+      </Switch>
+    </BrowserRouter>
+  </MainLayout>
 );
 
 export default App;
