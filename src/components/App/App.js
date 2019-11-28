97f0c3bfed839c8a4b80bdc095d215a1a173517d
Marysia Pabich <marysia.pabich@gmail.com>
2019-11-28 20:17:49 +0100

    Add transition effect using AnimatedSwitch component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 import Header from '../Header/Header';
 import FAQ from '../FAQ/FAQ';
+import { AnimatedSwitch } from 'react-router-transition';
+import styles from './App.scss';
 
 
 const App = () => (
   <MainLayout>
     <BrowserRouter>
       <Header />
-      <Switch>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={FAQ} />
-      </Switch>
+      </AnimatedSwitch>
     </BrowserRouter>
   </MainLayout>
 );
